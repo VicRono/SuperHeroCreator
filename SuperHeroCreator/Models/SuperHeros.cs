c41cb796888bb613f3e3a1d4889b94075ecd9958
unknown <v.rono@icloud.com>
2019-02-01 10:53:20 -0600

    Superhero model Created

---
+++ SuperHeroCreator/Models/SuperHeros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroCreator.Models
+{
+    public class SuperHeros
+    {
+        public string SuperHeroName { get; set; }
+        public string HerosAlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+
+    }
+}
